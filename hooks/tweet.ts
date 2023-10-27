1983fcb22ba0d3b48c9604f898f62ebde5855b28
vishalaitha <vishalaithaprod@gmail.com>
2023-10-27 18:51:32 +0530

    Tweet creation bug has been fixed

--- hooks/tweet.ts
+++ hooks/tweet.ts
@@ -5,18 +5,19 @@ import { getAllTweetsQuery } from "@/graphql/query/tweet";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
-
 export const useCreateTweet = () => {
   const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: (payload: CreateTweetData) =>
       graphqlClient.request(createTweetMutation, { payload }),
-    onMutate: () => toast.loading("Creating Tweet", { id: "before-after" }) ,
+    onMutate: (payload) => toast.loading("Creating Tweet", { id: "1" }),
     onSuccess: async (payload) => {
-      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
-      toast.success('Tweeted',{id:"before-after"});  
-    }
+      await queryClient.invalidateQueries({queryKey:["all-tweets"]});
+      toast.success("Tweeted", { id: "1" });
+    },
   });
+
   return mutation;
 };
 
