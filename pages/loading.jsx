97e77c9bbf9abe6fd601e413704b7a2cc5e46562
vishalaitha <vishalaithaprod@gmail.com>
2023-11-01 23:41:14 +0530

    Fixed a few bugs

---
+++ pages/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading=()=>{
+    return(
+        <section className="py-24">
+            <div className="container">
+                <h2 className="text-2xl">Loading...</h2>
+            </div>
+        </section>
+    )
+}
+export default Loading
